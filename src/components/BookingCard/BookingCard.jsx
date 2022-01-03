072ab7492fef20876774cc37663f47570fe8cae1
Walther <walther.vergaray@gmail.com>
2022-01-03 15:42:22 -0500

    adding functionallity to booking

--- src/components/BookingCard/BookingCard.jsx
+++ src/components/BookingCard/BookingCard.jsx
@@ -1,93 +1,108 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'prop-types';
 import './BookingCard.css';
 import { FormControl, TextField } from '@mui/material';
-import { getRooms } from '../../data';
 
-import { useUserContext } from '../../context/UserProvider';
-
-const BookingCard = () => {
-  // llamado del estado del contexto
-  const { addCart, setAddCart } = useUserContext();
-
-  const [rooms, setRooms] = useState([]);
-
-  useEffect(() => {
-    setRooms(getRooms);
-  }, []);
-
-  const roomsSelect = rooms.filter((room) =>
-    addCart.find((element) => element === room.id),
-  );
-
-  const hadlerClick = (roomIdDelete) => {
-    const roomsNotDelete = roomsSelect.filter(
-      (room) => room.id !== roomIdDelete,
-    );
-
-    const idRoomsNotDelete = roomsNotDelete.map((room) => room.id);
-    setAddCart(idRoomsNotDelete);
-  };
-
+const BookingCard = ({ cartRooms }) => {
   return (
     <div>
       <hr />
-      {roomsSelect.map((roomsel) => (
-        <div>
-          <div className="bookingCard" key={roomsel.id}>
-            <img
-              src="https://definicion.de/wp-content/uploads/2019/12/habitacion.jpg"
-              alt=""
-              className="bookingCard__image"
-            />
-            <div className="bookingCard__description">
-              <p className="bookingCard__type">{roomsel.type}</p>
-              <h3 className="bookingCard__title">{roomsel.title}</h3>
-              <p className="bookingCard__address">
-                {roomsel.address.street}, {roomsel.address.city},{' '}
-                {roomsel.address.province}, {roomsel.address.country}.
-              </p>
+      {cartRooms
+        ? cartRooms.map((roomsel) => (
+            <div key={roomsel._id}>
+              <div className="bookingCard">
+                <img
+                  src="https://definicion.de/wp-content/uploads/2019/12/habitacion.jpg"
+                  alt=""
+                  className="bookingCard__image"
+                />
+                <div className="bookingCard__description">
+                  <p className="bookingCard__type">{roomsel.hotel.category}</p>
+                  <h3 className="bookingCard__title">{roomsel.title}</h3>
+                  <p className="bookingCard__address">
+                    {roomsel.hotel.address.street}, {roomsel.hotel.address.city}
+                    , {roomsel.hotel.address.province},{' '}
+                    {roomsel.hotel.address.country}.
+                  </p>
 
-              <div className="bookingCard__footer">
-                {roomsel.tags.map((tag) => {
-                  return (
-                    <div className="tag__item" key={tag}>
-                      <i className="fa fa-envelope" />
-                      <p className="tag__text">{tag}</p>
-                    </div>
-                  );
-                })}
+                  <div className="bookingCard__footer">
+                    {roomsel.services.map((service) => {
+                      return (
+                        <div className="tag__item" key={service._id}>
+                          <i className={service.serviceUrl} />
+                          <p className="tag__text">{service.serviceName}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <form action="" style={{ marginTop: 20 }}>
+                    <FormControl>
+                      <TextField
+                        label="Nombre del Huésped"
+                        color="primary"
+                        variant="outlined"
+                        size="small"
+                        name="location"
+                        style={{ backgroundColor: 'white' }}
+                      />
+                    </FormControl>
+                  </form>
+                  <div className="bookingCard__button-container">
+                    <button type="button" className="bookingCard__button">
+                      <i className="fa fa-trash" />
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
               </div>
-              <form action="" style={{ marginTop: 20 }}>
-                <FormControl>
-                  <TextField
-                    label="Nombre del Huésped"
-                    color="primary"
-                    variant="outlined"
-                    size="small"
-                    name="location"
-                    style={{ backgroundColor: 'white' }}
-                  />
-                </FormControl>
-              </form>
-              <div className="bookingCard__button-container">
-                <button
-                  type="button"
-                  onClick={() => {
-                    hadlerClick(roomsel.id);
-                  }}
-                  className="bookingCard__button"
-                >
-                  <i className="fa fa-trash" />
-                  Eliminar
-                </button>
-              </div>
+              <hr />
             </div>
-          </div>
-          <hr />
-        </div>
-      ))}
+          ))
+        : ''}
     </div>
   );
+};
+
+BookingCard.propTypes = {
+  cartRooms: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      images: PropTypes.arrayOf(
+        PropTypes.shape({
+          imageName: PropTypes.string,
+          imageUrl: PropTypes.string,
+        }),
+      ),
+      services: PropTypes.arrayOf(
+        PropTypes.shape({
+          serviceName: PropTypes.string,
+          serviceUrl: PropTypes.string,
+        }),
+      ),
+      price: PropTypes.number,
+      capacity: PropTypes.number,
+      hotel: PropTypes.shape({
+        address: PropTypes.shape({
+          street: PropTypes.string,
+          city: PropTypes.string,
+          province: PropTypes.string,
+          country: PropTypes.string,
+        }),
+        name: PropTypes.string,
+        category: PropTypes.string,
+        phone: PropTypes.string,
+        email: PropTypes.string,
+        user: PropTypes.string,
+      }),
+    }),
+  ).isRequired,
+  searchFields: PropTypes.shape({
+    location: PropTypes.string,
+    capacity: PropTypes.number,
+  }).isRequired,
 };
 
 export default BookingCard;
