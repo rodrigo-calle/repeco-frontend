ff1d6498e67af623e8258fa735d6c17994ca9119
jilson jimenez <jilsonjimenez@gmail.com>
2022-01-16 20:07:25 -0500

    test hamburguer with props

--- src/components/HamburgerMenu/HamburgerMenu.test.js
+++ src/components/HamburgerMenu/HamburgerMenu.test.js
@@ -13,10 +13,24 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 // the component to test
 import HamburgerMenu from './HamburgerMenu';
+import { BrowserRouter } from 'react-router-dom';
 
+const linkmenu = [
+  {
+    title: 'Dashboard',
+    url: 'hola',
+    icon: 'fas fa-cube',
+  },
+  {
+    title: 'Estadisticas',
+    url: 'stadistics',
+    icon: 'fas fa-chart-pie',
+  },
+];
 
 test('menu have a title text', () => {
-  render(<HamburgerMenu/>);
-  const linkElement = screen.getByText(/REPECO/i);
+
+  render(<BrowserRouter><HamburgerMenu menu={linkmenu} /></BrowserRouter>);
+  const linkElement = screen.getByText(/Estadisticas/i);
   expect(linkElement).toBeInTheDocument();
 });
